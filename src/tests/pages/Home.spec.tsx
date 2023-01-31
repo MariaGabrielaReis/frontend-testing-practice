d65a6f652adb4b5eef8a24c06f63d46434fd45a2
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-01-31 10:39:36 -0300

    test: create test to function that loads initial data

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
-import Home from "../../pages";
+import { stripe } from "../../services/stripe";
+import Home, { getStaticProps } from "../../pages";
 
 jest.mock("next/router");
 jest.mock("next-auth/client", () => {
@@ -7,11 +8,33 @@ jest.mock("next-auth/client", () => {
     useSession: () => [null, false],
   };
 });
+jest.mock("../../services/stripe");
 
 describe("Home page", () => {
   it("renders correctly", () => {
     render(<Home product={{ priceId: "fake-price-id", amount: "R$ 21,64" }} />);
 
     expect(screen.getByText("for R$ 21,64 month")).toBeInTheDocument();
+  });
+
+  it("loads initial data", async () => {
+    const retrieveStripePricesMocked = jest.mocked(stripe.prices.retrieve);
+    retrieveStripePricesMocked.mockResolvedValueOnce({
+      id: "fake-price-id",
+      unit_amount: 2164,
+    } as any);
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: "fake-price-id",
+            amount: "$21.64",
+          },
+        },
+      }),
+    );
   });
 });
